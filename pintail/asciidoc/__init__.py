97c5e94062d2e940e3be1f6aeca3d1719b010bc9
Shaun McCance <shaunm@redhat.com>
2016-05-27 12:33:45 -0400

    Initial commit of pintail-asciidoc plugin

---
+++ pintail/asciidoc/__init__.py
@@ -0,0 +1,50 @@
+# pintail - Build static sites from collections of Mallard documents
+# Copyright (c) 2016 Shaun McCance <shaunm@gnome.org>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; see the file COPYING.LGPL.  If not, write to the
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02111-1307, USA.
+
+import os
+import subprocess
+from lxml import etree
+
+import pintail.site
+
+class AsciiDocPage(pintail.site.MallardPage, pintail.site.ToolsProvider):
+    def __init__(self, directory, source_file):
+        pintail.site.MallardPage.__init__(self, directory, source_file)
+
+    @classmethod
+    def build_tools(cls, site):
+        pass
+
+    def stage_page(self):
+        pintail.site.Site._makedirs(self.directory.stage_path)
+        subprocess.call(['asciidoctor-mallard',
+                         '-o', self.stage_path,
+                         self.source_path])
+
+    @property
+    def stage_file(self):
+        if self.source_file.endswith('.adoc'):
+            return self.source_file[:-5] + '.page'
+        else:
+            return self.source_file
+
+    @classmethod
+    def get_pages(cls, directory, filename):
+        if filename.endswith('.adoc'):
+            return [AsciiDocPage(directory, filename)]
+        return []
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pintail-asciidoc',
+    version='0.2',
+    description='Use AsciiDoc on Pintail sites.',
+    packages=['pintail', 'pintail.asciidoc'],
+    namespace_packages=['pintail'],
+    install_requires=['pintail>=0.2'],
+    author='Shaun McCance',
+    author_email='shaunm@gnome.org',
+    license='GPLv2+',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Text Processing :: Markup :: XML',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
+    ],
+)
