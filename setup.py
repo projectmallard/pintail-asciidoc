134b1cb1740f1b84447dafe8c5ed3f5cf48fa13f
Shaun McCance <shaunm@redhat.com>
2016-09-08 12:12:56 -0400

    setup.py: Check for python 3

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+import sys
+
+if sys.version_info[0] != 3:
+    sys.stderr.write("pintail-asciidoc requires python 3\n")
+    sys.exit(1)
+
 setup(
     name='pintail-asciidoc',
     version='0.3',
